b4dc0e4786b7e191c8a7a755c9d8801b3bfe926a
JDuncan97 <47870542+JDuncan97@users.noreply.github.com>
2020-04-27 12:08:33 -0500

    Update home.js

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -100,9 +100,9 @@ export default {
             console.log("Open EPA Data", response.data);
         });
         //TODO: Be able to get the user input for startDate and endDate
-        /*epaData.getHistoricalData(startDate, endDate).then(response => {
+        epaData.getHistoricalData(data.startDate, data.endDate).then(response => {
             console.log("Get EPA Historical Data", response.data);
-        });*/
+        });
     },
     mounted: function () {
         /** Let's first build the layers. Notice that map is not ready yet.
